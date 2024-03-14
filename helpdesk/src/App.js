6a3ee4acddfbcec10746d2344f4971552a8fa53f
Kidron Lightfoot <kidron.light99@gmail.com>
2024-03-14 09:03:59 -0700

    commit to change create react app boilerplate

---
+++ helpdesk/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, useRouteMatch } from 'react-router-dom';
+import './App.css';
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World 🌎</h1>
+    </div>
+  );
+}
