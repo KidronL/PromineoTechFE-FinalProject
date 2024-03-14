4e1e413f1303262b8bf458140e21dc47664a162d
Kidron Lightfoot <38261315+KidronL@users.noreply.github.com>
2024-03-14 14:55:34 -0700

    Adding Sass and changing router structure

--- helpdesk/src/App.js
+++ helpdesk/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useRouteMatch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 
