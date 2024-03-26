1db63da4837ecfb7e0e1b6cf82240086b8081c46
Kidron Lightfoot <38261315+KidronL@users.noreply.github.com>
2024-03-26 14:10:55 -0700

    Commit for router function

--- helpdesk/src/App.js
+++ helpdesk/src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Users from './routes/users/users.component';
 import Inventory from './routes/inventory/inventory.component';
 import './App.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 
 export default function App() {
   return (
-   <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/inventory" element={<Inventory />} />
-    </Routes>
+  <div className="App">
+    <Link to="/home">
+      Home
+    </Link>
+    <Link to="/users">
+      Users
+    </Link>
+    <Link to="/inventory">
+      Inventory
+    </Link>
+
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/inventory">
+          <Inventory />
+        </Route>
+      </Switch>
+    </div>
   );
 }
